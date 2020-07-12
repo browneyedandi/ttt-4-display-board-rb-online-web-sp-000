d7c60575cdfd298d03d0433d486036b644a3499a
Andriana Brockman <andriana.brockman@gmail.com>
2020-07-12 02:08:07 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,5 +1,9 @@
 def display_board(board)
 
  board = [" "," "," "," "," "," "," "," "," "]
- 
+  puts "   |   |   "
+  puts "-----------"
+  puts "   |   |   "
+  puts "-----------"
+  puts "   |   |   "
 end
