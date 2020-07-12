7f89a965d4e21594d83ba48a22e79670afb387ce
Andriana Brockman <andriana.brockman@gmail.com>
2020-07-12 02:20:37 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,9 +1,9 @@
 def display_board(board)
 
  board = [" "," "," "," "," "," "," "," "," "]
-  puts "#{[1]}|#{[1]}|#{[1]}"
+  puts "#{board[0]}|#{board[1]}|#{board[2]}"
   puts "-----------"
-  puts "#{[1]}|#{[1]}|#{[1]}"
+  puts "#{board[3]}|#{board[4]}|#{board[5]}"
   puts "-----------"
-  puts "#{[1]}|#{[1]}|   "
+  puts "#{board[6]}|#{board[7]}|#{board[8]}"
 end
